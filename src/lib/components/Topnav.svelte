efbde8151274b8f650513736e4a4cc5ab1675453
Marc Hyeler <birdpump@gmail.com>
2023-10-09 19:42:45 +0000

    add things to topnav

--- src/lib/components/Topnav.svelte
+++ src/lib/components/Topnav.svelte
@@ -1,17 +1,31 @@
 <div class="top">
-    <a href="/"><img alt="cv logo" class="logo" src="/CVHS-logo.png" /></a>
-    <div class="top-text">CVHS Locker System</div>
-
-    <div class="icon-stack">
-        <div class="material-symbols-outlined">search</div>
-        <div class="stack-text">Loookup</div>
+    <div class="logo-cont">
+        <a href="/"><img alt="cv logo" class="logo" src="/CVHS-logo.png" /></a>
+        <div class="top-text">CVHS Locker System</div>
     </div>
 
-    <div class="icon-stack">
-        <div class="material-symbols-outlined">home</div>
-        <div class="stack-text">Home</div>
+    <div class="icon-cont">
+        <a href="/">
+            <div class="icon-stack">
+                <div class="material-symbols-outlined">home</div>
+                <div class="stack-text">Home</div>
+            </div>
+        </a>
+
+        <a href="/register">
+            <div class="icon-stack">
+                <div class="material-symbols-outlined">person_add</div>
+                <div class="stack-text">Register</div>
+            </div>
+        </a>
+
+        <a href="/lookup">
+            <div class="icon-stack">
+                <div class="material-symbols-outlined">search</div>
+                <div class="stack-text">Lookup</div>
+            </div>
+        </a>
     </div>
-    
 </div>
 
 <svelte:head>
@@ -22,37 +36,86 @@
 </svelte:head>
 
 <style>
-    .icon-stack{
+    a:link {
+        text-decoration: none;
+    }
+
+    a:visited {
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+
+    a:active {
+        text-decoration: none;
+    }
+
+    :root {
+        --text: #d6d6d6;
+        --background: #101014;
+        --primary: #0084ff;
+        --secondary: #1b2c42;
+        --accent: #577db2;
+    }
+
+    .icon-stack {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
+        gap: 4px;
         cursor: pointer;
     }
 
-    .stack-text{
+    /* .icon-stack:hover{
+        background-color: #101014;
+        border-radius: 6px;
+    } */
+
+    .stack-text {
         /* font-family: Mo; */
         font-size: 14px;
         color: var(--text);
     }
 
     .material-symbols-outlined {
-        font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
+        font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 24;
         color: var(--text);
         user-select: none;
         cursor: pointer;
     }
 
+    .icon-cont {
+        margin-right: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 35px;
+    }
+
+    .logo-cont {
+        margin-left: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 25px;
+    }
+
     .top {
+        font-family: "Montserrat", sans-serif;
         width: 100vw;
         display: flex;
-        background-color: #1b2c42;
-        justify-content: center;
-        align-items: center;
+        background-color: #101014;
+        /* background-color: #1b2c42; */
+        justify-content: space-between;
         flex-direction: row;
-        gap: 15px;
+        /* gap: 15px; */
         height: 60px;
-        
+        /* border-bottom: 2px solid grey; */
     }
 
     .logo {
