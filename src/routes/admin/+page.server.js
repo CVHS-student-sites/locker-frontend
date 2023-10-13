70cb3be644652ea72f99725d10e9690e01d06817
Marc Hyeler <birdpump@gmail.com>
2023-10-13 17:31:22 +0000

    test cookie

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,26 +1,34 @@
 import { redirect } from '@sveltejs/kit';
 
-export async function load({ request }) {
-    const authToken = request.headers.get('Authorization');
+// export async function load({ request }) {
+//     const authToken = request.headers.get('Authorization');
 
-    console.log(request)
-    try {
-        console.log("hello")
-        const response = await fetch('http://mysql-container/auth/checkAuth', {
-            headers: {
-                'Authorization': authToken
-            }
-        });
-        console.log(response.ok)
-        if (!response.ok) {
-            throw redirect(302, '/login');
-        }
+//     console.log(request)
+//     try {
+//         console.log("hello")
+//         const response = await fetch('http://locker-backend:3000/auth/checkAuth', {
+//             headers: {
+//                 'Authorization': authToken
+//             }
+//         });
+//         console.log(response.ok)
+//         if (!response.ok) {
+//             throw redirect(302, '/login');
+//         }
 
-        // Continue loading the route as usual
-        return { props: {} };
+//         // Continue loading the route as usual
+//         return { props: {} };
 
-    } catch (error) {
-        console.error("Error checking authentication:", error);
-        throw redirect(302, '/login');
-    }
+//     } catch (error) {
+//         console.error("Error checking authentication:", error);
+//         throw redirect(302, '/login');
+//     }
+// }
+
+export function load({ cookies }) {
+	const visited = cookies.get('connect.sid');
+
+	// cookies.set('visited', 'true', { path: '/' });
+
+	console.log(visited)
 }
