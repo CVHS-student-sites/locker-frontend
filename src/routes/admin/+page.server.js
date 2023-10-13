c7b566f3365c7f988c720fc34ef4be15d1a86995
Marc Hyeler <birdpump@gmail.com>
2023-10-13 01:14:51 +0000

    make admin check auth

---
+++ src/routes/admin/+page.server.js
@@ -0,0 +1,25 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ request }) {
+    const authToken = request.headers.get('Authorization');
+
+    try {
+        console.log("hello")
+        const response = await fetch('https://locker-api.cvapps.net/auth/checkAuth', {
+            headers: {
+                'Authorization': authToken
+            }
+        });
+        console.log(response.ok)
+        if (!response.ok) {
+            throw redirect(302, '/login');
+        }
+
+        // Continue loading the route as usual
+        return { props: {} };
+
+    } catch (error) {
+        console.error("Error checking authentication:", error);
+        throw redirect(302, '/login');
+    }
+}
