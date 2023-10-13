38f8d293a2e74eb91a85152ff1120bf221f602ed
Marc Hyeler <birdpump@gmail.com>
2023-10-13 17:41:57 +0000

    fix logout

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -4,41 +4,11 @@
     import { goto } from '$app/navigation';
 
 
-    // Function to check authentication status
-    async function checkAuthentication() {
-        try {
-            // Make a GET request to the server's /checkauth endpoint
-            const response = await fetch('https://locker-api.cvapps.net/auth/checkauth', {
-                method: 'GET',
-                credentials: 'include', // Include cookies in the request
-            });
-
-
-
-            if (response.ok) {
-                // User is authenticated
-                let resp = await response.json();
-
-                console.log(resp.user.username)
-
-                alert(resp.user.username)
-                return true;
-            } else {
-                // User is not authenticated, handle accordingly
-                await goto('/login');
-                return false;
-            }
-        } catch (error) {
-            // Handle any network or other errors here
-            console.error('Error checking authentication:', error);
-            return false;
-        }
-    }
 
     async function logo() {
         try {
             // Make a GET request to the server's /checkauth endpoint
-            const response = await fetch('https://locker-api.cvapps.net/logout', {
+            const response = await fetch('https://locker-api.cvapps.net/auth/logout', {
                 method: 'POST',
                 credentials: 'include', // Include cookies in the request
             });
@@ -67,14 +37,6 @@
     let isAuthenticated = false;
 
     // You might want to use the onMount lifecycle function
-    import { onMount } from 'svelte';
-
-    onMount(async () => {
-       await checkAuthentication();
-
-        // await login();
-    });
-
 
     // The rest of your component logic for the protected route
 
