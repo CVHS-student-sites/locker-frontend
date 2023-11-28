2644a0a2fae9d064691e5fe2aa027d4d4e75b9f7
birdpump <birdpump@gmail.com>
2023-11-27 16:14:43 -0800

    add csv test

--- src/routes/admin/dash/+page.svelte
+++ src/routes/admin/dash/+page.svelte
@@ -1,1 +1,14 @@
-<h1>hello thest on dash</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+<body>
+<form action="https://locker-api.cvapps.net/admin/upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="csvFile" accept=".csv" required>
+    <button type="submit">Upload</button>
+</form>
+</body>
+</html>
