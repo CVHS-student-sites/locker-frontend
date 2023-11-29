5548e15d2e605a718dca695f0eb644d787d34f07
Marc Hyeler <birdpump@gmail.com>
2023-11-28 21:52:34 -0800

    shit that took a while

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -1,26 +1,26 @@
 import { redirect } from '@sveltejs/kit';
 
-export async function load({ cookies }) {
-    const auth = cookies.get('connect.sid');
-
-    
-    try {
-        const response = await fetch('http://locker-backend:3000/auth/checkAuth', {
-            credentials: 'include', // Include credentials (cookies) with the request
-            headers: {
-                'Cookie': `connect.sid=${auth}`, // Set the Cookie header with the cookie value
-            },
-        });
-
-        if (!response.ok) {
-            throw redirect(302, '/login');
-        }
-
-        // Continue loading the route as usual
-        return { props: {} };
-
-    } catch (error) {
-        console.error("Error checking authentication:", error);
-        throw redirect(302, '/login');
-    }
-}
+// export async function load({ cookies }) {
+//     const auth = cookies.get('connect.sid');
+//
+//
+//     try {
+//         const response = await fetch('http://locker-backend:3000/auth/checkAuth', {
+//             credentials: 'include', // Include credentials (cookies) with the request
+//             headers: {
+//                 'Cookie': `connect.sid=${auth}`, // Set the Cookie header with the cookie value
+//             },
+//         });
+//
+//         if (!response.ok) {
+//             throw redirect(302, '/login');
+//         }
+//
+//         // Continue loading the route as usual
+//         return { props: {} };
+//
+//     } catch (error) {
+//         console.error("Error checking authentication:", error);
+//         throw redirect(302, '/login');
+//     }
+// }
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,45 +1,242 @@
-<script>
-    // src/routes/admin.svelte (or layout.svelte)
-    import { redirect } from '@sveltejs/kit';
-    import { goto } from '$app/navigation';
 
+<svelte:head>
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+            rel="stylesheet"
+    />
+</svelte:head>
+<style>
+    .material-symbols-outlined {
+        font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
+        color: #577db2;
+        user-select: none;
+        cursor: pointer;
+        width: 24px;
 
+    }
 
-    async function logo() {
-        try {
-            // Make a GET request to the server's /checkauth endpoint
-            const response = await fetch('https://locker-api.cvapps.net/auth/logout', {
-                method: 'POST',
-                credentials: 'include', // Include cookies in the request
-            });
+    .main {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh; /* Ensure the container takes at least the full height of the viewport */
+        background-color: var(--background);
+    }
 
+    .top {
+        display: flex;
+        height: 60px;
+        padding: 0 12vw;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+        z-index: 9999;
+        background-color: var(--background);
+        align-items: center;
+    }
 
+    .logo-cont {
+        display: flex;
+        align-items: center;
+        gap: 15px;
+        /*width: 100%;*/
+        height: 100%;
+        padding: 5px;
+        box-sizing: border-box;
+    }
 
-            if (response.ok) {
-                // User is authenticated
-                // await goto('/login');
-                return true;
-            } else {
-                // User is not authenticated, handle accordingly
+    .top-logo {
+        height: 100%;
+    }
 
-                return false;
-            }
-        } catch (error) {
-            // Handle any network or other errors here
-            console.error('Error checking authentication:', error);
-            return false;
-        }
+    .logo-text {
+        font-size: 16px;
+        font-family: 'Montserrat', sans-serif;
+        color: #d6d6d6;
+    }
+
+    .center {
+        gap: 16px;
+        display: flex;
+        flex: 1; /* Expand to fill remaining vertical space */
+        margin: 20px 12vw; /* Adjust the margin as needed */
+    }
+
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        width: 20%;
+        background-color: #18181b;
+        border-radius: 10px;
+        padding: 16px;
+        box-sizing: border-box;
+        gap: 5px;
+    }
+
+    .active-button{
+        background-color: #003566;
+    }
+
+    .side-element-1{
+        width: 100%;
+        height: 60px;
+        padding: 16px;
+        box-sizing: border-box;
+        border-radius: 5px;
+        transition: background-color 0.1s ease;
+        margin-bottom: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 16px;
+    }
+
+    .side-element-1:hover{
+        background-color: #404040;
+        cursor: pointer;
+    }
+
+    .user-text{
+        font-size: 16px;
+        font-family: 'Montserrat', sans-serif;
+        color: #d6d6d6;
     }
 
 
+    .side-element-2{
+        width: 100%;
+        height: 50px;
+        padding: 16px;
+        box-sizing: border-box;
+        border-radius: 50px;
+        transition: background-color 0.1s ease;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 16px;
+    }
 
-    // Check authentication status when the component loads
-    let isAuthenticated = false;
+    .side-element-2:hover{
+        background-color: #404040;
+        cursor: pointer;
+    }
 
-    // You might want to use the onMount lifecycle function
+    .button-text{
+        font-size: 16px;
+        font-family: 'Montserrat', sans-serif;
+        color: #d6d6d6;
+    }
 
-    // The rest of your component logic for the protected route
 
-</script>
+    .grid-cont {
+        flex: 1; /* Expand to fill remaining horizontal space */
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: 2fr 3fr 2fr; /* Adjust the fr values as needed */
+        box-sizing: border-box;
+    }
 
-<button class="submit" on:click={logo}>Sign in</button>
+    .grid-element-1 {
+        width: 100%;
+        height: 100%;
+        background-color: #1b2c42;
+        border-radius: 10px;
+        padding: 8px 12px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        text-align: center;
+    }
+
+    .large {
+        grid-column: span 3;
+        background-color: #18181b;
+    }
+
+    .medium{
+        grid-column: span 2;
+        background-color: #18181b;
+    }
+</style>
+
+
+<div class="main">
+
+    <div class="top">
+        <div class="logo-cont">
+            <img alt="CVHS falcon logo" class="top-logo" src="/CVHS-logo.png">
+            <div class="logo-text">CVHS Locker System</div>
+        </div>
+    </div>
+
+    <div class="center">
+        <div class="sidebar">
+            <div class="side-element-1">
+                <div class="material-symbols-outlined">account_circle</div>
+                <div class="user-text">Birdpump</div>
+            </div>
+
+            <div class="side-element-2 active-button">
+                <div class="material-symbols-outlined">data_usage</div>
+                <div class="button-text">Overview</div>
+            </div>
+            <div class="side-element-2">
+                <div class="material-symbols-outlined">list</div>
+                <div class="button-text">Edit</div>
+            </div>
+
+            <div class="side-element-2">
+                <div class="material-symbols-outlined">tune</div>
+                <div class="button-text">Management</div>
+            </div>
+
+            <div class="side-element-2">
+                <div class="material-symbols-outlined">settings</div>
+                <div class="button-text">Settings</div>
+            </div>
+        </div>
+
+        <div class="grid-cont">
+            <div class="grid-element-1">
+                <div class="button-text">Total users</div>
+                <div class="button-text">2,563</div>
+                <div class="button-text">+15%</div>
+            </div>
+            <div class="grid-element-1">
+                <div class="button-text">Total lockers</div>
+                <div class="button-text">1,253</div>
+                <div class="button-text">+8%</div>
+            </div>
+            <div class="grid-element-1">
+                <div class="button-text">last 30 days</div>
+                <div class="button-text">+27 lockers</div>
+                <div class="button-text">+35%</div>
+            </div>
+            <div class="grid-element-1">
+                <div class="button-text">Last 30 days</div>
+                <div class="button-text">+20 users</div>
+                <div class="button-text">+18%</div>
+            </div>
+
+            <div class="grid-element-1 large">
+                <div class="button-text">graph of some sort</div>
+            </div>
+            <div class="grid-element-1">
+                <div class="button-text">list names of new users</div>
+            </div>
+
+            <div class="grid-element-1 medium">
+                <div class="button-text">buttons to control something like freeze registration</div>
+            </div>
+            <div class="grid-element-1 medium">
+                <div class="button-text">more buttons to do stuff, idk lol</div>
+            </div>
+        </div>
+    </div>
+</div>
