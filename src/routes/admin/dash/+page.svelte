bdf3d5bb0c1f2497c6bad7c61b6002e2f7efc7fc
birdpump <birdpump@gmail.com>
2023-11-28 17:20:31 -0800

    add new upload route

--- src/routes/admin/dash/+page.svelte
+++ src/routes/admin/dash/+page.svelte
@@ -6,9 +6,16 @@
     <title>File Upload</title>
 </head>
 <body>
-<form action="https://locker-api.cvapps.net/admin/upload" method="post" enctype="multipart/form-data">
+<form action="https://locker-api.cvapps.net/admin/lockerUpload" method="post" enctype="multipart/form-data">
     <input type="file" name="csvFile" accept=".csv" required>
-    <button type="submit">Upload</button>
+    <button type="submit">Upload Locker</button>
 </form>
+<br>
+
+<form action="https://locker-api.cvapps.net/admin/userUpload" method="post" enctype="multipart/form-data">
+    <input type="file" name="csvFile" accept=".csv" required>
+    <button type="submit">Upload User</button>
+</form>
+
 </body>
 </html>
