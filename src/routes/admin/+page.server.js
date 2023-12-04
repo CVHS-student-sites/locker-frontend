ba60522fa05fc8d879a4f17d1d81221500a30bd8
Marc Hyeler <birdpump@gmail.com>
2023-12-04 09:58:26 -0800

    Update +page.server.js

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -3,27 +3,35 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ cookies }) {
     // console.log(process.env.NODE_ENV);
     console.log(import.meta.env.PROD);
-    
-    const auth = cookies.get('connect.sid');
-    
 
-    try {
-        const response = await fetch('http://locker-backend:3000/auth/checkAuth', {
-            credentials: 'include', // Include credentials (cookies) with the request
-            headers: {
-                'Cookie': `connect.sid=${auth}`, // Set the Cookie header with the cookie value
-            },
-        });
-
-        if (!response.ok) {
+    //check if on prod
+    if(import.meta.env.PROD){
+        
+        //get auth cookie
+        const auth = cookies.get('connect.sid');
+        try {
+            const response = await fetch('http://locker-backend:3000/auth/checkAuth', {
+                credentials: 'include', // Include credentials (cookies) with the request
+                headers: {
+                    'Cookie': `connect.sid=${auth}`, // Set the Cookie header with the cookie value
+                },
+            });
+    
+            if (!response.ok) {
+                throw redirect(302, '/login');
+            }
+    
+            // Continue loading the route as usual
+            return { props: {} };
+    
+        } catch (error) {
+            console.error("Error checking authentication:", error);
             throw redirect(302, '/login');
         }
-
-        // Continue loading the route as usual
+    }else{
+        //return route if not in prod without checking auth
         return { props: {} };
-
-    } catch (error) {
-        console.error("Error checking authentication:", error);
-        throw redirect(302, '/login');
     }
+    
+    
 }
