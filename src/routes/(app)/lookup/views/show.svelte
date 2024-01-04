d5a64b11c144f39939d5dd34e0c1b0d60c252e8a
birdp <birdpump@gmail.com>
2024-01-03 19:40:46 -0800

    fix root layout bug

---
+++ src/routes/(app)/lookup/views/show.svelte
@@ -0,0 +1,224 @@
+<script>
+
+
+    export let data;
+
+    $: test = data;
+
+    // import Topnav from "$lib/components/Topnav.svelte";
+    import {goto} from '$app/navigation';
+
+    let username = "";
+    let password = "";
+
+    function handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            // Enter key was pressed, do something
+            submit();
+        }
+    }
+
+
+    import { createEventDispatcher } from "svelte";
+    import { fade } from 'svelte/transition';
+
+    const dispatch = createEventDispatcher();
+    console.log(data)
+
+</script>
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+</svelte:head>
+
+<style>
+    :root {
+        --text: #d6d6d6;
+        --background: #101014;
+        --primary: #0084ff;
+        --secondary: #1b2c42;
+        --accent: #577db2;
+    }
+
+
+    .main {
+        font-family: 'Montserrat', sans-serif;
+        background-color: var(--background);
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+    }
+
+    .logo {
+        height: 70px;
+        cursor: pointer;
+    }
+
+    .top-text {
+        font-size: 18px;
+        color: var(--text);
+    }
+
+    .login {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        row-gap: 30px;
+    }
+
+    .login-cont {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border-radius: 8px;
+        /*border: 2px solid black;*/
+        /*width: 416px;*/
+        box-shadow: 0px 18px 18px rgba(0, 0, 0, 0.15), 0 0 18px rgba(0, 0, 0, 0.15);
+        padding: 32px;
+        color: green;
+
+        /*background: linear-gradient(14deg, rgba(2,0,36,1) 0%, rgba(101,62,165,1) 100%);*/
+        /*background: linear-gradient(14deg, #182435 0%, #577db2 100%);*/
+        /* background-color:#131d2a; */
+        background-color: #1b2c42;
+    }
+
+    .login-header {
+        font-size: 24px;
+        color: var(--text);
+        margin-bottom: 12px;
+    }
+
+    .login-form {
+        display: flex;
+        justify-content: center;
+
+        width: 352px;
+        flex-direction: column;
+        /*align-items: center;*/
+        row-gap: 10px;
+    }
+
+
+    label {
+        color: var(--text);
+        line-height: 20px;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .forgot {
+        color: #4ca6ff;
+    }
+
+    input[type=text],
+    input[type=password] {
+        width: 100%;
+        padding: 0px 8px 0px 8px;
+        box-sizing: border-box;
+        height: 35px;
+        line-height: 35px;
+        color: var(--text);
+        border: 2px solid #005cb3;
+        /* border: none; */
+        background-color: #17171c;
+
+        border-radius: 4px;
+        transition-duration: 50ms;
+        font-size: 14px;
+    }
+
+    input:focus {
+        outline: none;
+        border-color: #577db2;
+        /*background-color: #eaeaea;*/
+    }
+
+    input:hover {
+        border-color: #577db2;
+        /*background-color: #eaeaea;*/
+    }
+
+    .submit {
+        width: 100%;
+        height: 35px;
+        background-color: #0082ff;
+        border: none;
+        border-radius: 4px;
+        font-weight: bold;
+        margin-top: 10px;
+        cursor: pointer;
+        transition-duration: 150ms;
+        color: var(--text);
+    }
+
+    .submit:hover {
+        background-color: #577db2;
+    }
+
+    .bottom-text {
+        width: 100%;
+        display: flex;
+        /*align-items: center;*/
+        justify-content: center;
+        column-gap: 5px;
+    }
+
+    .reg {
+        color: #fbfdfe;
+    }
+
+    .regs {
+        color: #4ca6ff;
+        cursor: pointer;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .main {
+            /* background: rgb(2, 0, 36); */
+            /* background: linear-gradient(14deg, rgb(9, 3, 121) 0%, rgb(33, 97, 239) 100%); */
+        }
+
+        .logo {
+            height: 35px;
+            /*left: 20px;*/
+        }
+
+        .login {
+            row-gap: 30px;
+        }
+
+        .login-cont {
+            width: 100vw;
+            box-shadow: none;
+            padding: 0;
+            background: unset;
+        }
+
+        .login-form {
+            width: 90vw;
+        }
+
+        input[type=text],
+        input[type=password] {
+            width: 100%;
+
+        }
+    }
+
+</style>
+<div class="main">
+
+    <div class="login">
+
+        <h1>{data.Locker.location.row}</h1>
+
+    </div>
+</div>
