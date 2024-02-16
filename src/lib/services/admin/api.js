95f64e1fac3c97948e523e043588974f8c8f03c1
birdpump <birdpump@gmail.com>
2024-02-16 12:23:49 -0800

    add api functions

--- src/lib/services/admin/api.js
+++ src/lib/services/admin/api.js
@@ -1,16 +1,9 @@
 export async function fetchOverviewData() {
-    try {
-        const response = await fetch('https://locker-api.cvapps.net/admin/management/area-restrictions', {
-            method: 'GET',
-        });
+    const response = await fetch('https://locker-api.cvapps.net/admin/management/area-restrictions', {
+        method: 'get',
+        credentials: 'include'
+    });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok.');
-        }
-
-        const data = await response.json(); // Assuming the response is JSON data
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching overview data:', error);
-    }
+    const data = await response.json(); // Assuming the response is JSON data
+    console.log(data);
 }
