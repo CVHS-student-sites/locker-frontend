604bb60e9ec2a9bddaeb33e7eebcd40383c843da
birdpump <birdpump@gmail.com>
2024-02-16 12:46:26 -0800

    fix api function

--- src/routes/(admin)/admin/dash/+page.svelte
+++ src/routes/(admin)/admin/dash/+page.svelte
@@ -6,6 +6,7 @@
         console.log(await fetchOverviewData())
     });
 </script>
+
 <form action="https://locker-api.cvapps.net/admin/lockerUpload" method="post" enctype="multipart/form-data">
     <input type="file" name="csvFile" accept=".csv" required>
     <button type="submit">Upload Locker</button>
