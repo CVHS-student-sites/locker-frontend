62e60919d2513574c8110b07771cd3cdff44fc23
Marc Hyeler <birdpump@gmail.com>
2024-05-15 15:48:42 +0000

    add credentials to request

--- src/routes/(admin)/admin/manage/UploadData.svelte
+++ src/routes/(admin)/admin/manage/UploadData.svelte
@@ -1,47 +1,25 @@
 <script>
-let files;
-import axios from "axios";
+    let files;
+    import axios from "axios";
 
-async function SubmitEvent(){
-    var formData = new FormData();
-    formData.append("csvFile", files);
+    async function SubmitEvent() {
+        var formData = new FormData();
+        formData.append("csvFile", files);
 
-    let response = await axios.post('https://locker-api.cvapps.net/admin/lockerUpload', formData, {
-        headers: {
-            'Content-Type': 'multipart/form-data'
+        let response = await axios.post("https://locker-api.cvapps.net/admin/lockerUpload", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            withCredentials: true,
+        });
+
+        if (response.status === 200) {
+            alert("form uploaded");
+        } else {
+            alert(response.status);
         }
-    });
-
-    if(response.status === 200){
-        alert("form uploaded");
-    }else{
-        alert(response.status);
     }
-
-
-}
-
 </script>
-
-
-<style>
-    .import-cont {
-        flex: 1;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        /* background-color: green; */
-        box-sizing: border-box;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .form {
-        width: 100%;
-        height: 100%;
-    }
-</style>
-
 
 <div class="import-cont">
     <div class="form">
@@ -61,23 +39,30 @@ async function SubmitEvent(){
 
         <input accept=".csv" bind:files id="locker" name="locker" type="file" />
         <button on:click={SubmitEvent}>Upload locker</button>
-
-
     </div>
 
     <div class="form">
-        <form
-                action="https://locker-api.cvapps.net/admin/userUpload"
-                enctype="multipart/form-data"
-                method="post"
-        >
-            <input
-                    accept=".csv"
-                    name="csvFile"
-                    required
-                    type="file"
-            />
+        <form action="https://locker-api.cvapps.net/admin/userUpload" enctype="multipart/form-data" method="post">
+            <input accept=".csv" name="csvFile" required type="file" />
             <button type="submit">Upload User</button>
         </form>
     </div>
 </div>
+
+<style>
+    .import-cont {
+        flex: 1;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        /* background-color: green; */
+        box-sizing: border-box;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .form {
+        width: 100%;
+        height: 100%;
+    }
+</style>
