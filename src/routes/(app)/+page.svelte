d14f30bcb786f6ff018273ad239978c292817c56
Marc Hyeler <birdpump@gmail.com>
2024-07-05 10:23:04 -0700

    add mobile parallax effect

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -65,7 +65,7 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 100%;
+        /*height: 100%;*/
         /*width: 50%;*/
     }
 
@@ -76,6 +76,17 @@
 
     p {
         color: var(--text);
+    }
+
+    @media screen and (max-width: 600px) {
+        .sub {
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+        }
     }
 </style>
 
