b4884def782583e32270304d17b9b9a34e3d10a7
Marc Hyeler <birdpump@gmail.com>
2024-08-12 15:10:02 -0700

    update debug panel

--- src/routes/(admin)/admin/debug/+page.svelte
+++ src/routes/(admin)/admin/debug/+page.svelte
@@ -5,13 +5,33 @@
 
     let logHtml = '';
 
+    let autoRefresh = false;
+
     onMount(async () => {
-        let test = await getDebugLogs()
-
-        const convert = new AnsiToHtml();
-
-        logHtml = test.map(log => convert.toHtml(log)).join('')
+        await updateLogs();
     });
+
+    let interval
+    $: if (autoRefresh) {
+        interval = setInterval(updateLogs, 1000);
+    }
+
+    $: if (!autoRefresh) {
+        clearInterval(interval);
+    }
+
+    async function updateLogs() {
+        let test = await getDebugLogs()
+        const convert = new AnsiToHtml();
+        logHtml = test.map(log => convert.toHtml(log)).join('')
+        await scrollToBottom(element);
+    }
+
+    let element;
+
+    const scrollToBottom = async (node) => {
+        node.scroll({top: node.scrollHeight, behavior: 'smooth'});
+    };
 </script>
 <svelte:head>
     <title>Locker - Debug</title>
@@ -40,11 +60,20 @@
         height: 100%;
         overflow-y: scroll;
     }
+
+    .check-text {
+        font-size: 12px;
+        font-family: "Montserrat", sans-serif;
+        color: #d6d6d6;
+    }
 </style>
 
 
 <div class="main">
-    <div class="log-entry">
+    <div class="top-menu">
+        <label class="check-text"><input type="checkbox" bind:checked={autoRefresh}/>Auto Refresh Logs</label>
+    </div>
+    <div class="log-entry" bind:this={element}>
         {@html logHtml}
     </div>
 </div>
